f0401748ce96c78ca88d84b629fef4c37cc1e09a
Yiming Huang <hym20030128@petalmail.com>
2023-05-15 02:13:40 +0000

    modified:   netfetch/bin/netfetch.dart

--- netfetch/bin/netfetch.dart
+++ netfetch/bin/netfetch.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 void main() async {
-  final httpPackageUrl = Uri.https('dart.dev', '/f/packages/http.json');
+  final httpPackageUrl = Uri.parse('https://raw.githubusercontent.com/preoge/netcatcher/main/mydate.json');
   final httpPackageInfo = await http.read(httpPackageUrl);
   print(httpPackageInfo);
 }
